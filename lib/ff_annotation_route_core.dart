17e2c0a4e4b5fe0bd6770e12522b42bf5831705c
zmtzawqlp <zmtzawqlp@live.com>
2021-02-23 22:58:03 +0800

    ## 1.1.0
    - Add FFArgumentImport

--- lib/ff_annotation_route_core.dart
+++ lib/ff_annotation_route_core.dart
@@ -1,3 +1,4 @@
 library ff_annotation_route_core;
 
+export 'src/ff_argument_import.dart';
 export 'src/ff_route.dart';
---
+++ lib/src/ff_argument_import.dart
@@ -0,0 +1,9 @@
+/// Annotation of argumentImport
+class FFArgumentImport {
+  const FFArgumentImport([this.suffix]);
+
+  /// The suffix of this import
+  /// for example, maybe you need to hide,show
+  /// hide xyz,show abc
+  final String suffix;
+}
